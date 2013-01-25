7f2c7967170d56e8cbd84399ad08ede96e6b3510
Arnaud Ladoucette <aladouce@emi.u-bordeaux1.fr>
2013-01-25 16:39:51 +0100

    TD1 d image

--- ImgSon/TD1/images.c
+++ ImgSon/TD1/images.c
@@ -21,6 +21,8 @@ typedef unsigned char ndgIm [LARGEUR][HAUTEUR];
 // Image couleur
 typedef unsigned char coulIm [LARGEUR][HAUTEUR][3];
 
+typedef float histo[256];
+
 
 // mode = "r" ou "w"
 void 
@@ -85,8 +87,7 @@ lireEnTete (FILE *descFic, char *mode)
 
 // Lecture d'une image en ndg: mise a jour de la 
 // structure de donnees de type ndgIm
-void 
-lireNdgImage (char *nom, ndgIm im)
+void lireNdgImage (char *nom, ndgIm im)
 {
   FILE *descFic = NULL;
   int c, x, y;
@@ -96,10 +97,10 @@ lireNdgImage (char *nom, ndgIm im)
 
   for (y=0; y < HAUTEUR; y++)  
     for (x=0; x < LARGEUR; x++)
-      {
-	fscanf (descFic, "%d", &c);
-	im [x][y] = c;
-      }
+    {
+		fscanf (descFic, "%d", &c);
+		im [x][y] = c;
+    }
   
   fclose (descFic);
 }
@@ -137,29 +138,142 @@ ecrireNdgImage (char *nom, ndgIm im)
   fclose (descFic);
 }
 
+// ~~~~ EXERCICE III ~~~~~~~
+
+void imageUnie(char* nom, int ng)
+{
+	FILE * newimg = fopen(nom, "w+");
+	ecrireEnTete(newimg, "P2");
+	
+	int x,y;
+	for (y=0; y<HAUTEUR; y++)
+	{
+		for (x=0; x<LARGEUR; x++)
+			fprintf(newimg, "%d ", ng);
+		fprintf(newimg, "\n");
+	}
+	
+	printf("Fichier %s créer !\n", nom);
+	fclose(newimg);
+}
+
+void degradeHorizontal(char * nom, int ng1, int ng2)
+{
+	FILE * newimg = fopen(nom, "w+");
+	ecrireEnTete(newimg, "P2");
+	
+	int x,y;
+	for (y=0; y<HAUTEUR; y++)
+	{
+		for (x=0; x<LARGEUR; x++)
+		{
+			float m1 = (LARGEUR*1.0-(x+1))/LARGEUR;
+			float m2 = (x+1)*1.0/LARGEUR;
+			int c = (m1*ng1) + (m2*ng2);
+			fprintf(newimg, "%d ", c);
+		}
+		//~ printf("\n");
+	}
+	printf("Fichier %s créer !\n", nom);
+	fclose(newimg);
+}
+
+// ~~~~~ EXERCICE IV ~~~~~~~~~
 
 
-/**********************/
-// Question 8.3
-/**********************/
-void histogramme_dat(char* hnom, histo h){ 
-  FILE *hfile = NULL;
-  ouvrirFichier (hnom, &hfile, "w");
-  int i; 
-  for (i = 0; i < VAL_MAX; i++) 
-    fprintf(hfile,"%d %f \n", i,h[i]); 
-  fclose(hfile);
-} 
+// Lecture d'une image en ndg: mise a jour de la 
+// structure de donnees de type ndgIm
+void lireCoulImage (char *nom, coulIm im)
+{
+  FILE *descFic = NULL;
+  int c, x, y, z;
+
+  ouvrirFichier (nom, &descFic, "r");
+  lireEnTete (descFic, "P3");
+
+  for (y=0; y < HAUTEUR; y++)  
+    for (x=0; x < LARGEUR; x++)
+    {
+		for (z=0; z<3; z++)
+		{
+			fscanf (descFic, "%d", &c);
+			im[x][y][z] = c;
+		}
+    }
+  
+  fclose (descFic);
+}
+
+// Ecrit une image en ndg
+void 
+ecrireCoulImage (char *nom, coulIm im)
+{
+  FILE *descFic = NULL;
+  int x, y, z;
+  
+  ouvrirFichier (nom, &descFic, "w+");
+  ecrireEnTete (descFic, "P3");
+  
+  for (y=0; y < HAUTEUR; y++)
+  {
+	for (x=0; x < LARGEUR; x++)
+		for (z=0; z<3; z++)
+			fprintf (descFic, "%d ", im[x][y][z]);
+	fprintf (descFic, "\n");
+  }
+
+  fclose (descFic);
+}
+
+// ~~~~~ EXERCICE V ~~~~~~~~~
+
+void RGBToGray(coulIm src, ndgIm dest)
+{
+	int x, y, z;
+	int rgb[3];
+	
+	for (y=0; y < HAUTEUR; y++)
+		for (x=0; x < LARGEUR; x++)
+		{
+			for (z=0; z<3; z++)
+				rgb[z] = src[x][y][z];
+			dest[x][y] = 0.3*rgb[0] + 0.59*rgb[1] + 0.11*rgb[2];
+		}
+}
+
+//~ /**********************/
+//~ // Question 8.3
+//~ /**********************/
+//~ void histogramme_dat(char* hnom, histo h){ 
+  //~ FILE *hfile = NULL;
+  //~ ouvrirFichier (hnom, &hfile, "w");
+  //~ int i; 
+  //~ for (i = 0; i < VAL_MAX; i++) 
+    //~ fprintf(hfile,"%d %f \n", i,h[i]); 
+  //~ fclose(hfile);
+//~ } 
 
 
 int 
 main ()
 {
-  ndgIm im;
+  coulIm im;
+  ndgIm d;
   
-  lireNdgImage ("brume.pgm", im);
-  ecrireNdgImage ("test.pgm", im);
+  lireCoulImage ("fleur.ppm", im);
+  
+   //~ ajout du pixel noir
+  //~ im[128][128][0] = 0;
+  //~ im[128][128][1] = 255;
+  //~ im[128][128][2] = 0;
+  
+  RGBToGray(im,d);
 
+  
+  ecrireNdgImage ("test.ppm", d);
+  degradeHorizontal("degrade.pgm", 75, 203);
+
+	imageUnie("gris.pgm", 50);
 
 
   return EXIT_SUCCESS;
