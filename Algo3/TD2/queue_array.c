8ca88b4c25c4321c5493cf1b3dea20c3c0ee3c99
arnaud33200 <arnaud33200@gmail.com>
2013-02-06 17:26:42 +0100

    Algo 3 : fin du TD 3

--- Algo3/TD2/queue_array.c
+++ Algo3/TD2/queue_array.c
@@ -19,18 +19,31 @@ void queue_grow(queue s)
 	int ns = s->lenght * 2;
 	void ** newtab = malloc(sizeof(void *) * ns);
 	int i;
-	for(i=0; i<ns; i++)
+	for(i=0; i<=s->back; i++)
 		newtab[i] = s->tab[i];
-	void ** old = s->tab;
+
+// si il reste encore un morceau de l'ancien tableau
+// copier le reste à la fin du nouveau tableau
+	if ( s->front > s-> back )
+	{
+		int p = ns - s->lenght;
+		for(i=s->front; i<s->lenght; i++)
+			newtab[i+p] = s->tab[i];
+	}
 	
+	free(s->tab);
+	s->tab = newtab;
+	s->lenght = ns;
+	printf("~~ The table has been grow !\n");
 }
 
 /* create an empty queue */
 queue queue_create(void)
 {
-	queue s = malloc(sizeof(queue));
+	queue s = malloc(sizeof(struct queue_t));
 	s->tab = malloc(sizeof(void *)*BLOCK);
-	s->back = s->front = 0;
+	s->back = 0;
+	s->front = 0;
 	s->lenght = BLOCK;
 	s->nbElement = 0;
 	return s;
@@ -45,24 +58,42 @@ void queue_destroy(queue s)
 /* return true if and only if the queue is empty */
 int queue_empty(queue s)
 {
-  return nbElement <= 0 ? 1 : 0;
+  return s->nbElement <= 0 ? 1 : 0;
 }
 
 /* push an object on the back of the queue */
 void queue_push(queue s, void *object)
 {
 	if( s->nbElement >= s->lenght )
-		grow
+		queue_grow(s);
+
+	s->tab[s->back] = object;
+	++s->back;
+	if(s->back >= s->lenght)
+		s->back = 0;
+	
+	++s->nbElement;
 }
 
 
 /* return the front element of the queue.
    The queue must not be empty (as reported by queue_empty()) */
-void * queue_front(queue s){
-  return NULL;
+void * queue_front(queue s)
+{
+  return queue_empty(s) == 0 ? s->tab[s->front] : NULL;
 }
 
 /* pop the front element off of the queue.
    The queue must not be empty (as reported by queue_empty()) */
-void queue_pop(queue s){
+void queue_pop(queue s)
+{
+	if( queue_empty(s) == 0 )
+	{
+		++s->front;
+		if ( s->front >= s->lenght)
+			s->front = 0;
+		--s->nbElement;
+	}
+	else
+		printf("## Pop impossible : table is empty\n");
 }
