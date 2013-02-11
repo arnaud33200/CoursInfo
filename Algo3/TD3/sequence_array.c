64f42304cf8add37989e25022ab8641f3f06edec
arnaud33200 <arnaud33200@gmail.com>
2013-02-11 19:12:08 +0100

    Algo3 : début TD3, implémentation array

--- Algo3/TD3/sequence_array.c
+++ Algo3/TD3/sequence_array.c
@@ -1,30 +1,135 @@
+#include<stdio.h>
+#include<stdlib.h>
+
 #include "sequence.h"
+
+const int BLOCK = 10;
+int first_empty_slot(sequence s);
+void grow_array(sequence s);
 
 
 struct sequence_t
 {
-	
+	void ** tab;
+	int length;
+	int size;
 };
 
 
 /* create an empty sequence */
-sequence sequence_create();
+sequence sequence_create()
+{
+	sequence new = malloc( sizeof(struct sequence_t));
+	new->tab = malloc(sizeof(void *) * BLOCK);
+	for (int i = 0; i < new->size; ++i)
+		new->tab[i] = NULL;
+	new->length = 0;
+	new->size = BLOCK;
+}
 
 /* destroy the whole sequence (not objects)*/
-void sequence_destroy(sequence s);
+void sequence_destroy(sequence s)
+{
+	free(s->tab);
+	free(s);
+}
 
 /* find the object at position pos and return it or NULL if not
    found */
-void * sequence_find(sequence s, int pos);
+void * sequence_find(sequence s, int pos)
+{
+	return sequence_length(s) <= 0 ? NULL : s->tab[pos]; 
+}
 
 /* insert an object between objects at position pos-1 and pos */
-void sequence_insert(sequence s, void * object, int pos);
+void sequence_insert(sequence s, void * object, int pos)
+{
+	if ( sequence_length(s) >= s->size || pos >= s->size )
+		grow_array(s);
+
+	int nulpos = first_empty_slot(s);
+	if ( nulpos == pos)
+		s->tab[pos] = object;
+	else if (nulpos < pos)
+	{
+		for (int i = nulpos; i < pos-1; ++i)
+			s->tab[i] = s->tab[i+1];
+		s->tab[pos-1] = object;
+	}
+	else if ( nulpos > pos)
+	{
+		for (int i = nulpos; i >= pos; --i)
+			s->tab[i] = s->tab[i-1];
+		s->tab[pos] = object;
+	}
+
+	++s->length;
+}
 
 /* delete the object at position pos */
-void sequence_delete(sequence s, int pos);
+void sequence_delete(sequence s, int pos)
+{
+	if( sequence_length(s) > 0)
+	{
+		s->tab[pos] = NULL;
+		--s->length;
+	}
+}
 
 /* dump the sequence as integer object */
-void sequence_dump(sequence s);
+void sequence_dump(sequence s)
+{
+	for (int i = 0; i < s->size; ++i)
+	{
+		if ( s->tab[i] == NULL)
+			printf(" %d[]-", i);
+		else
+			printf(" %d[% s]-", i, s->tab[i]);
+	}
+	printf("\n\n");
+}
 
 /* return the sequence length */
-int sequence_length(sequence s);
+int sequence_length(sequence s)
+{
+	int l = s->length;
+	return s->length;
+}
+
+
+int first_empty_slot(sequence s)
+{
+	int find = 0;
+	int nulpos = 0;
+	int i = 0;
+	while ( find == 0 && i < s->size )
+	{
+		if ( s->tab[i] == NULL )
+		{
+			find = 1;
+			nulpos = i;
+		}
+		++i;
+	}
+	return nulpos;
+}
+
+void grow_array(sequence s)
+{
+	int ns = s->size * 2;
+	void ** newtab = malloc(sizeof(void *) * ns);
+	int i;
+	for(i=0; i<=s->size; i++)
+		newtab[i] = s->tab[i];
+	
+	free(s->tab);
+	s->tab = newtab;
+
+	for (int i = s->size; i < ns; ++i)
+		s->tab[i] = NULL;
+
+	s->size = ns;
+
+
+	printf("~~ The table has been grow !\n");
+}
--- Algo3/TD3/test_sequence.c
+++ Algo3/TD3/test_sequence.c
@@ -10,14 +10,14 @@ void main (int argc, char* argv[])
   int pos = 0;
   int length = 0;
   sequence s = sequence_create();
-  srand(time(NULL))  
+  srand(time(NULL));
    
   while(length < 20) {
     char * c = malloc(sizeof(char));
     *c = getc(stdin);
     if(*c == '\n') continue;
     pos = (int) (length * (rand() / (RAND_MAX + 1.0)));
-    printf("try to insert %d at pos %d...\n",*c,pos);
+    printf("try to insert %s at pos %d...\n",c,pos);
     sequence_insert(s,c,pos);
     length++;
     sequence_dump(s);
