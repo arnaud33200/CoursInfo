7c7dbf50322062adaff448676d1c78fb4793a975
Arnaud Ladoucette <aladouce@emi.u-bordeaux1.fr>
2013-02-11 14:44:07 +0100

    ALgo 3: td3 debut

---
+++ Algo3/TD3/sequence_array.c
@@ -0,0 +1,30 @@
+#include "sequence.h"
+
+
+struct sequence_t
+{
+	
+};
+
+
+/* create an empty sequence */
+sequence sequence_create();
+
+/* destroy the whole sequence (not objects)*/
+void sequence_destroy(sequence s);
+
+/* find the object at position pos and return it or NULL if not
+   found */
+void * sequence_find(sequence s, int pos);
+
+/* insert an object between objects at position pos-1 and pos */
+void sequence_insert(sequence s, void * object, int pos);
+
+/* delete the object at position pos */
+void sequence_delete(sequence s, int pos);
+
+/* dump the sequence as integer object */
+void sequence_dump(sequence s);
+
+/* return the sequence length */
+int sequence_length(sequence s);
---
+++ Algo3/TD3/test_sequence.c
@@ -0,0 +1,27 @@
+/* test_sequence.c */
+
+#include "sequence.h"
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void main (int argc, char* argv[]) 
+{
+  int pos = 0;
+  int length = 0;
+  sequence s = sequence_create();
+  srand(time(NULL))  
+   
+  while(length < 20) {
+    char * c = malloc(sizeof(char));
+    *c = getc(stdin);
+    if(*c == '\n') continue;
+    pos = (int) (length * (rand() / (RAND_MAX + 1.0)));
+    printf("try to insert %d at pos %d...\n",*c,pos);
+    sequence_insert(s,c,pos);
+    length++;
+    sequence_dump(s);
+  }
+
+  sequence_destroy(s);
+}
